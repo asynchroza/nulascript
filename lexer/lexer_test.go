f4d41ae53946a3f5a0fd03c9f8523749e1aad603
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-08-16 21:02:18 +0300

    Refactor GetNextToken text for easier expanding

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,37 +6,48 @@ import (
 )
 
 func TestGetNextToken(t *testing.T) {
-	input := `=+-,;(){}*&|`
-
-	tokenTests := []struct {
-		expectedType    token.TokenType
-		expectedLiteral string
+	testCases := []struct {
+		input      string
+		tokenTests []struct {
+			expectedType    token.TokenType
+			expectedLiteral string
+		}
 	}{
-		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
-		{token.MINUS, "-"},
-		{token.COMMA, ","},
-		{token.SEMICOLON, ";"},
-		{token.LPAR, "("},
-		{token.RPAR, ")"},
-		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.POINTER, "*"},
-		{token.DEREF, "&"},
-		{token.PIPE, "|"},
+		{`=+-,;(){}*&|`,
+			[]struct {
+				expectedType    token.TokenType
+				expectedLiteral string
+			}{
+				{token.ASSIGN, "="},
+				{token.PLUS, "+"},
+				{token.MINUS, "-"},
+				{token.COMMA, ","},
+				{token.SEMICOLON, ";"},
+				{token.LPAR, "("},
+				{token.RPAR, ")"},
+				{token.LBRACE, "{"},
+				{token.RBRACE, "}"},
+				{token.POINTER, "*"},
+				{token.DEREF, "&"},
+				{token.PIPE, "|"},
+			},
+		},
 	}
 
-	l := NewLexer(input)
+	for _, testCase := range testCases {
+		l := NewLexer(testCase.input)
 
-	for iter, testToken := range tokenTests {
-		var incomingToken token.Token = l.GetNextToken()
+		for iter, testToken := range testCase.tokenTests {
+			var incomingToken token.Token = l.GetNextToken()
 
-		if incomingToken.Type != testToken.expectedType {
-			t.Fatalf("[%d]: EXPECTED TOKEN: %q, RECEIVED TOKEN: %q", iter, testToken.expectedType, incomingToken.Type)
-		}
+			if incomingToken.Type != testToken.expectedType {
+				t.Fatalf("[%d]: EXPECTED TOKEN: %q, RECEIVED TOKEN: %q", iter, testToken.expectedType, incomingToken.Type)
+			}
 
-		if incomingToken.Literal != testToken.expectedLiteral {
-			t.Fatalf("[%d]: EXPECTED LITERAL: %q, RECEIVED LITERAL: %q", iter, testToken.expectedLiteral, incomingToken.Literal)
+			if incomingToken.Literal != testToken.expectedLiteral {
+				t.Fatalf("[%d]: EXPECTED LITERAL: %q, RECEIVED LITERAL: %q", iter, testToken.expectedLiteral, incomingToken.Literal)
+			}
 		}
 	}
+
 }
