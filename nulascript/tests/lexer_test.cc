2eb605995efd2ea0e13a51e7a5209fba1e2044fb
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-09-21 15:21:14 +0300

    Add second test case for basic functionality

--- nulascript/tests/lexer_test.cc
+++ nulascript/tests/lexer_test.cc
@@ -2,6 +2,8 @@
 #include "token.h"
 #include "lexer.h"
 
+#define MULTILINE_STRING(s) #s
+
 TEST(LexerSuite, LexerTest) {
     std::vector<std::pair<std::string, std::vector<Token>>> testCases = {
         {
@@ -21,10 +23,55 @@ TEST(LexerSuite, LexerTest) {
                 {TokenType::PIPE, "|"},
             },
         },
+        {
+            MULTILINE_STRING(let numOne = 10;
+            let numTwo = 20;
+			let add = fn(x, y){
+				x + y;
+			}
+
+			let result = add(numOne, numTwo);),
+            {
+                {TokenType::LET, "let"},
+				{TokenType::IDENT, "numOne"},
+				{TokenType::ASSIGN, "="},
+				{TokenType::INT, "10"},
+				{TokenType::SEMICOLON, ";"},
+				{TokenType::LET, "let"},
+				{TokenType::IDENT, "numTwo"},
+				{TokenType::ASSIGN, "="},
+				{TokenType::INT, "20"},
+				{TokenType::SEMICOLON, ";"},
+				{TokenType::LET, "let"},
+				{TokenType::IDENT, "add"},
+				{TokenType::ASSIGN, "="},
+				{TokenType::FUNC, "fn"},
+				{TokenType::LPAR, "("},
+				{TokenType::IDENT, "x"},
+				{TokenType::COMMA, ","},
+				{TokenType::IDENT, "y"},
+				{TokenType::RPAR, ")"},
+				{TokenType::LBRACE, "{"},
+				{TokenType::IDENT, "x"},
+				{TokenType::PLUS, "+"},
+				{TokenType::IDENT, "y"},
+				{TokenType::SEMICOLON, ";"},
+				{TokenType::RBRACE, "}"},
+				{TokenType::LET, "let"},
+				{TokenType::IDENT, "result"},
+				{TokenType::ASSIGN, "="},
+				{TokenType::IDENT, "add"},
+				{TokenType::LPAR, "("},
+				{TokenType::IDENT, "numOne"},
+				{TokenType::COMMA, ","},
+				{TokenType::IDENT, "numTwo"},
+				{TokenType::RPAR, ")"},
+            }
+        }
     };
 
     for (const auto& testCase : testCases) {
-            Lexer lexer = Lexer(testCase.first);
+            Lexer lexer(testCase.first);
 
         for (const auto& expectedToken : testCase.second) {
             Token actualToken = lexer.getNextToken();
